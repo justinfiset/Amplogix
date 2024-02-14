b71a69b1ad9d61810c769d5ba3808874327f31d7
Gabriel Riverin <2244454@cegeplimoilou.ca>
2024-02-14 16:21:50 -0500

    Ouverture browser file et création de nouveau projet

---
+++ Assets/Scripts/Main Menu/MainMenuButtons.cs
@@ -0,0 +1,26 @@
+using SFB;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void CreateProject()
+    {
+        SceneManager.LoadScene("CircuitCreator", LoadSceneMode.Single);
+    }
+   
+
+    public void LoadModel(/*Model model*/)
+    {
+        // TODO: LoadModel
+    }
+
+    public void OpenFile()
+    {    
+        var paths = StandaloneFileBrowser.OpenFilePanel("Ouvrir fichier", "", ".amp", false);
+
+    }
+}
