ed8382e698deaab945f279641b249c8f67e8cc4e
Gabriel lebel <2239650@cegeplimoilou.ca>
2024-02-14 17:25:52 -0500

    Drag et ProjectManager

--- Assets/Scripts/Circuit/ProjectManager.cs
+++ Assets/Scripts/Circuit/ProjectManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ProjectManager : MonoBehaviour
 {
+    public TextMeshProUGUI nameText;
+    
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class ProjectManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        nameText.text = "bitchass";
     }
+   
 }
