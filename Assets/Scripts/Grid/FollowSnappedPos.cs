90598b889df7f079087a73100ff2b012ff0203d0
Justin Fiset <justinfiset@hotmail.fr>
2024-02-21 16:00:43 -0500

    Fix main menu corrompu

---
+++ Assets/Scripts/Grid/FollowSnappedPos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSnappedPos : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position = GridSettings.GetCurrentSnapedPosition();
+    }
+}
