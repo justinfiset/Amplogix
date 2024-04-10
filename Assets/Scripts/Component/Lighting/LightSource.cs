8542868a679338d6ea4f68e524756e2e084bd1a6
alexis miron <2240773@cegeplimoilou.ca>
2024-04-10 16:17:53 -0400

    permet de disable la lumière

--- Assets/Scripts/Component/Lighting/LightSource.cs
+++ Assets/Scripts/Component/Lighting/LightSource.cs
@@ -6,31 +6,42 @@ public class LightSource : MonoBehaviour
 {
     public GameObject visualLightPrefab;
     [HideInInspector] public VisualLight visualLight;
-    public int opacityMultiplier;
+    public float opacityMultiplier;
     public Color lightColor;
+
+    public bool doLighting;
 
     // Start is called before the first frame update
     void Start()
     {
+        CreateLight();
+        if (!doLighting)
+        {
+            visualLight.gameObject.SetActive(false);
+        }
+    }
+
+    private void CreateLight()
+    {
         visualLight = Instantiate(visualLightPrefab, transform).GetComponent<VisualLight>();
+        Vector3 tmp = visualLight.transform.position;
+        tmp.z += 1;
+        visualLight.transform.position = tmp;
         visualLight.SetRGBColor(lightColor);
         UpdateLighting();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnIntensityChange()
     {
-        UpdateLighting();
+        if (doLighting)
+        {
+            UpdateLighting();
+        }
     }
 
     public void UpdateLighting()
     {
-        visualLight.SetOpacity(GetIntensity());
+        visualLight.SetOpacity(GetIntensity() * opacityMultiplier);
     }
 
     private float GetIntensity()
