6c390a1c563e20b314eb8c8a9a4f7c2cdca63c67
Justin Fiset <justinfiset@hotmail.fr>
2024-04-25 22:41:29 -0400

    MEGA optimisation du GUI

--- Assets/Scripts/Component/GUI/ComponentGUI.cs
+++ Assets/Scripts/Component/GUI/ComponentGUI.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class ComponentGUI : MonoBehaviour
+public class ComponentGUI
 {
+    private static bool isInitiated = false;
+
+    public static Color[] colorList = null;
+    public static GUIStyle[] colorStyle = null;
+    public static int colorRowCount = 2;
+    public static int colorColCount = 5;
+
     public static GUIStyle currentStyle = null;
     public static GUIStyle buttonStyle = null;
     public static GUIStyle deleteStyle = null;
@@ -11,52 +19,73 @@ public class ComponentGUI : MonoBehaviour
     public static GUIStyle inputStyle = null;
     public static ComponentGUILayout currentLayout;
 
-    public static GUIStyle InitGUI(float heightDivider = 3f)
+    public static GUIStyle InitGUI()
     {
-        int fontSize = 32;
-        GUI.skin.box.fontSize = fontSize;
-        GUI.skin.button.fontSize = fontSize;
-        InitLayout(heightDivider);
-        return InitStyles();
+        if (!isInitiated)
+        {
+            int fontSize = 32;
+            GUI.skin.box.fontSize = fontSize;
+            GUI.skin.button.fontSize = fontSize;
+            return InitStyles();
+        }
+        else return currentStyle;
     }
 
-    public static ComponentGUILayout InitLayout(float heightDivider)
+    public static ComponentGUILayout InitLayout(float heightDivider = 3f)
     {
-        currentLayout = new ComponentGUILayout(heightDivider);
+        if(currentLayout == null || heightDivider != currentLayout.yDividier)
+        {
+            currentLayout = new ComponentGUILayout(heightDivider);
+        }
         return currentLayout;
     }
 
     public static GUIStyle InitStyles()
     {
-        currentStyle = new GUIStyle(GUI.skin.box);
-        currentStyle.normal.background = MakeTex(2, 2, Color.white);
-        currentStyle.normal.textColor = Color.black;
+        if(currentStyle == null)
+        {
+            currentStyle = new GUIStyle(GUI.skin.box);
+            currentStyle.normal.background = MakeTex(2, 2, Color.white);
+            currentStyle.normal.textColor = Color.black;
+        }
 
-        buttonStyle = new GUIStyle(GUI.skin.box);
-        buttonStyle.alignment = TextAnchor.MiddleCenter;
-        buttonStyle.normal.background = MakeTex(2, 2, new Color32(49, 106, 189, 255));
-        buttonStyle.normal.textColor = Color.white;
-        buttonStyle.hover.background = MakeTex(2, 2, new Color32(20, 56, 105, 255));
-        buttonStyle.hover.textColor = Color.white;
-        buttonStyle.active.background = MakeTex(2, 2, new Color32(19, 34, 56, 255));
-        buttonStyle.active.textColor = Color.white;
+        if(buttonStyle == null)
+        {
+            buttonStyle = new GUIStyle(GUI.skin.box);
+            buttonStyle.alignment = TextAnchor.MiddleCenter;
+            buttonStyle.normal.background = MakeTex(2, 2, new Color32(49, 106, 189, 255));
+            buttonStyle.normal.textColor = Color.white;
+            buttonStyle.hover.background = MakeTex(2, 2, new Color32(20, 56, 105, 255));
+            buttonStyle.hover.textColor = Color.white;
+            buttonStyle.active.background = MakeTex(2, 2, new Color32(19, 34, 56, 255));
+            buttonStyle.active.textColor = Color.white;
+        }
 
-        deleteStyle = new GUIStyle(GUI.skin.box);
-        deleteStyle.alignment = TextAnchor.MiddleCenter;
-        deleteStyle.normal.background = MakeTex(2, 2, new Color32(191, 54, 54, 255));
-        deleteStyle.normal.textColor = Color.white;
-        deleteStyle.hover.background = MakeTex(2, 2, new Color32(125, 30, 30, 255));
-        deleteStyle.hover.textColor = Color.white;
-        deleteStyle.active.background = MakeTex(2, 2, new Color32(59, 12, 9, 255));
-        deleteStyle.active.textColor = Color.white;
+        if(deleteStyle == null)
+        {
+            deleteStyle = new GUIStyle(GUI.skin.box);
+            deleteStyle.alignment = TextAnchor.MiddleCenter;
+            deleteStyle.normal.background = MakeTex(2, 2, new Color32(191, 54, 54, 255));
+            deleteStyle.normal.textColor = Color.white;
+            deleteStyle.hover.background = MakeTex(2, 2, new Color32(125, 30, 30, 255));
+            deleteStyle.hover.textColor = Color.white;
+            deleteStyle.active.background = MakeTex(2, 2, new Color32(59, 12, 9, 255));
+            deleteStyle.active.textColor = Color.white;
+        }
 
-        labelStyle = new GUIStyle(currentStyle);
-        labelStyle.alignment = TextAnchor.MiddleCenter;
+        if (labelStyle == null)
+        {
+            labelStyle = new GUIStyle(currentStyle);
+            labelStyle.alignment = TextAnchor.MiddleCenter;
+        }
 
-        inputStyle = new GUIStyle(buttonStyle);
-        inputStyle.alignment = TextAnchor.MiddleLeft;
-        inputStyle.padding.left = (int)currentLayout.padding;
-        inputStyle.normal.background = MakeTex(2, 2, new Color32(52, 104, 179, 255));
+        if (inputStyle == null)
+        {
+            inputStyle = new GUIStyle(buttonStyle);
+            inputStyle.alignment = TextAnchor.MiddleLeft;
+            inputStyle.padding.left = (int)currentLayout.padding;
+            inputStyle.normal.background = MakeTex(2, 2, new Color32(52, 104, 179, 255));
+        }
 
         return currentStyle;
     }
@@ -102,46 +131,58 @@ public class ComponentGUI : MonoBehaviour
         return new Rect(x, y, width, height);
     }
 
-    private static Color[] GenerateColorList()
+    private static void GenerateColorList()
     {
-        List<Color> list = new List<Color>();
-        list.Add(new Color32(255, 105, 97, 255)); // rouge 
-        list.Add(new Color32(255, 180, 128, 255)); // oragne 
-        list.Add(new Color32(248, 243, 141, 255)); // jaune
-        list.Add(new Color32(66, 214, 164, 255)); // vert
-        list.Add(new Color32(8, 202, 209, 255)); // turquoise
-        list.Add(new Color32(89, 173, 246, 255)); // bleu
-        list.Add(new Color32(157, 148, 255, 255)); // mauve
-        list.Add(new Color32(199, 128, 232, 255)); // rose
-        list.Add(new Color32(255, 255, 255, 255)); // blanc
-        list.Add(new Color32(0, 0, 0, 255)); // noir
-        return list.ToArray();
+        if (colorList == null)
+        {
+            List<Color> list = new List<Color>();
+            list.Add(new Color32(255, 105, 97, 255)); // rouge 
+            list.Add(new Color32(255, 180, 128, 255)); // oragne 
+            list.Add(new Color32(248, 243, 141, 255)); // jaune
+            list.Add(new Color32(66, 214, 164, 255)); // vert
+            list.Add(new Color32(8, 202, 209, 255)); // turquoise
+            list.Add(new Color32(89, 173, 246, 255)); // bleu
+            list.Add(new Color32(157, 148, 255, 255)); // mauve
+            list.Add(new Color32(199, 128, 232, 255)); // rose
+            list.Add(new Color32(255, 255, 255, 255)); // blanc
+            list.Add(new Color32(0, 0, 0, 255)); // noir
+            colorList = list.ToArray();
+        }
+
+        if(colorStyle == null)
+        {
+            colorStyle = new GUIStyle[10];
+            for(int i = 0; i < colorList.Length; i++)
+            {
+                GUIStyle style = new GUIStyle(GUI.skin.box);
+                style.normal.background = MakeTex(2, 2, colorList[i]);
+                colorStyle[i] = style;
+            }
+        }
     }
 
     public static void CreateColorPalette()
     {
-        Color[] colors = GenerateColorList();
-        int rowCount = 2;
-        int colCount = 5;
+        if(colorList == null)
+        {
+            GenerateColorList();
+        }
 
-        float size = (currentLayout.width - 2 * currentLayout.padding) / colCount;
+        float size = (currentLayout.width - 2 * currentLayout.padding) / colorColCount;
 
         // 2 row of 5 cokumns
         int count = 0;
-        for(int i = 0; i < rowCount; i++)
+        for(int i = 0; i < colorRowCount; i++)
         {
             float y = currentLayout.y + currentLayout.height - 40 - 2 * currentLayout.padding - 2 * size + (i * size);
-            for (int j = 0; j < colCount; j++)
+            for (int j = 0; j < colorColCount; j++)
             {
-                GUIStyle style = new GUIStyle(GUI.skin.box);
-                style.normal.background = MakeTex(2, 2, colors[count]);
-
                 float x = currentLayout.x + j * size + currentLayout.padding;
                 Rect rect = new Rect(x, y, size, size);
 
-                if (GUI.Button(rect, "", style))
+                if (GUI.Button(rect, "", colorStyle[count]))
                 {
-                    ProjectManager.ChangeSelectionColor(colors[count]);
+                    ProjectManager.ChangeSelectionColor(colorList[count]);
                 }
                 count++;
             }
@@ -151,6 +192,7 @@ public class ComponentGUI : MonoBehaviour
 
 public class ComponentGUILayout
 {
+    public float yDividier;
     public float padding = 20f;
     public float height;
     public float width;
@@ -159,6 +201,7 @@ public class ComponentGUILayout
 
     public ComponentGUILayout(float heightDivider)
     {
+        yDividier = heightDivider;
         height = Screen.height / heightDivider;
         width = Screen.width / 4;
         x = Screen.width - width - padding;
